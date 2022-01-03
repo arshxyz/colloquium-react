da79287fab611fc7b7a6b0805ea28c541c100c02
Arsh Kohli <kohliarsh9@gmail.com>
2022-01-03 21:15:43 +0530

    initial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg type="circle" num={2} color="#ff0000" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -68,10 +68,10 @@ class Header extends Component {
             <Fade bottom duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                  <i className="fa fa-trophy"></i>Our Achievements
                 </a>
                 <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                  <i className="fa fa-info-circle"></i>About us
                 </a>
               </ul>
             </Fade>
