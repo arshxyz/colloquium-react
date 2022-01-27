9ae9bac8ee62186e2b9ad6a54c73500ff8c26b19
Shyamal <08.shyamal@gmail.com>
2022-01-28 01:30:29 +0530

    aboutusimg

--- src/Components/AboutUs/AboutUsPage.jsx
+++ src/Components/AboutUs/AboutUsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutUsPage.css';
-import cover from './AboutUsImage/varsity.jpeg';
+import cover from './AboutUsImage/img.jpg';
 import logo from './AboutUsImage/debsoc.png';
 import { NavBar } from "../Nav";
 import { Footer } from "../Footer";
@@ -17,9 +17,10 @@ function AboutUs() {
                 <AboutUsContent/>
             </div>
             <hr></hr>
+            <br></br>
             <div className='Team'>
                 <div className='our'>
-                    <h1>Our Team </h1>
+                    <h1>Meet The Team </h1>
                 </div>
                 <OurTeam></OurTeam>
             </div>
--- src/Components/AboutUs/Card.jsx
+++ src/Components/AboutUs/Card.jsx
@@ -3,14 +3,11 @@ import './card.css';
 
 const Card=({imageUrl,name,position,testimonial})=>(
     <div className='card'>
-    <div class="profile-card-4 text-center">
-    <img src={imageUrl} class="img img-responsive"/>
+    <img src={imageUrl} className="img"/>
     <div class="profile-content">
            <div className='position'> 
            <p>{name}</p>
            <p className='pos'>{position}</p> </div>
-        <div class="profile-description">{testimonial}</div>
-    </div>
     </div>
     </div>
 )
