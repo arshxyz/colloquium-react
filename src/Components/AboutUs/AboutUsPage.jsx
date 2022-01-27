ef3c18a3666aa675ad6c88698df1246f63af570e
Shyamal <08.shyamal@gmail.com>
2022-01-27 23:57:45 +0530

    added about

--- src/Components/AboutUs/AboutUsPage.jsx
+++ src/Components/AboutUs/AboutUsPage.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import './AboutUsPage.css';
-import AboutusText from './AboutUsText/aboutUsText';
-import Aboutimg from './AboutUsImage/aboutus.image';
-import Title from './AboutUsTitle/aboutus.title';
-import AboutUsButton from './AboutUsButton/AboutUsButton';
-import AboutUsSubheading from './AboutUsSubHead/subhead';
-import { NavBar } from '../Nav';
+import cover from './AboutUsImage/varsity.jpeg';
+import logo from './AboutUsImage/debsoc.png';
+import { NavBar } from "../Nav";
+import { Footer } from "../Footer";
+import AboutUsContent from './AboutUsContent';
+import OurTeam from './ourTeam/ourTeam';
 
-const AboutUs = () => (
-	<>
-    <NavBar/>
-
-	<div>
-		<div className="image">
-			<Aboutimg link="https://images.unsplash.com/photo-1533035336122-4327d347d2fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhaW58ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" />
-		</div>
-		<div className="flex-container">
-			<div className="flex-child title">
-				<Title>Who are we?</Title>
-				<AboutusText />
-				<AboutUsButton className="about-button" />
-			</div>
-			<div className="flex-child subhead">
-				<AboutUsSubheading />
-			</div>
-		</div>
-	</div>
-	</>
-);
+function AboutUs() {
+    return (
+        <div >
+            <NavBar />
+            <img alt="hi" src={cover} className='cover'></img>
+            <div className='content'>
+                <img alt="hi" src={logo} className='logo'></img>
+                <AboutUsContent/>
+            </div>
+            <hr></hr>
+            <div className='Team'>
+                <div className='our'>
+                    <h1>Our Team </h1>
+                </div>
+                <OurTeam></OurTeam>
+            </div>
+            <footer />
+        </div>
+    );
+}
 
 export default AboutUs;
---
+++ src/Components/AboutUs/teamdata.js
@@ -0,0 +1,132 @@
+import arjun from './Photos/Arjun.jpeg';
+import rahul from './Photos/Rahul.jpg';
+import kanav from './Photos/Kanav.jpg';
+import ananya from './Photos/Ananya.jpg';
+import yajas from './Photos/Yajas.jpg';
+import mitansh from './Photos/Mitansh.jpg';
+import nandni from './Photos/Nandni.jpg';
+import pranay from './Photos/Pranay.jpg';
+import anav from './Photos/Anav.jpg';
+import kartikeya from './Photos/Kartikeya.jpg';
+import arpan from './Photos/Arpan.jpg';
+import pragati from './Photos/Pragati.jpeg';
+import shreyans from './Photos/Shreyans.jpeg';
+import avil from './Photos/Avil.jpg';
+import aditi from './Photos/Aditi.jpg';
+import sai from './Photos/Sai.JPG';
+
+const team=[
+    {
+        id:1,
+        name:'Arjun Singh',
+        position:'President',
+        imageUrl:arjun,
+        testimonial:'Being one of the most active and thriving societies across the debating circuit, DebSoc NSUT is a stranger to none. Whether it be winning countless accolades every year or organizing Delhi’s largest debating and literary festival, we really are able to do it all. DebSoc isn’t just a society for us, it is also an experience filled with sleepless nights and rewarding days.'
+    },
+    {
+        id:2,
+        name:'Rahul Sharma',
+        position:'President',
+        imageUrl:rahul,
+        testimonial:'DebSoc NSUT is a platform that not only provides a safe space for deliberation and discussion, but is a home away from home, a family where everyone works on motivating others to be the best versions of themselves, in all aspects of life. With days spent debating, and nights spent reminiscing on those memories and making new ones, DebSoc is truly the most wholesome society I’ve had an opportunity to be a part of.'
+    },
+    {
+        id:3,
+        name:'Kanav Dwevedi',
+        position:'Vice President',
+        imageUrl:kanav,
+        testimonial:'The Debating Society has long fostered a constructive and welcoming environment for debate and learning. Simply put, our vision is to be a successful study led organisation, a model which can be emulated by other such organisations. Year after year we have realised our vision with our members winning several reputed national and international awards and organising several popular debating fests and competitions.'
+    },
+    {
+        id:4,
+        name:'Ananya Gupta',
+        position:'Vice President',
+        imageUrl:ananya,
+        testimonial:'DebSoc NSUT is the place to be if you want to share your views, discuss something, or even if you want to rant about college life. We at DebSoc NSUT meet at CBT(mostly) and sometimes in the Nescii Lawns and then we discuss issues ranging from major world problems involving economics and international relations to philosophical and moral debates which force you to think deeply, we do it all with utmost grit and conviction while having fun along the way. '
+    },
+    {
+        id:5,
+        name:'Yajas Sardana',
+        position:'Executive Director',
+        imageUrl:yajas,
+        testimonial:'DebSoc is the place where one can unlock their true potential. From discussing and debating over every minute to major issues for hours, every major world problem has been solved on our very favourite venue, CBT. DebSoc NSUT is where one can find the most hardworking people who have a passion to learn. It is more than a student society, DebSoc is a family.'
+    },
+    {
+        id:6,
+        name:'Mitansh Khurana',
+        position:'Executive Director',
+        imageUrl:mitansh,
+        testimonial:'DebSoc NSUT is not your regular student club/society you would have anticipated to find in college. It is a community of like-minded people who strive to improve every day. For a community that follows DebSoc meets by DebSoccers grit, the society would always be welcoming to every student in the university who has a zeal to grow, with the society essentially ameliorating the university life.'   
+    },
+    {
+        id:7,
+        name:'Nandni Srivastava',
+        position:'Head,Technology and Design',
+        imageUrl:nandni,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'   
+    },
+    {
+        id:8,
+        name:'Pranay Kothari',
+        position:'Head,Technology and Design',
+        imageUrl:pranay,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:9,
+        name:'Anav Choudhary',
+        position:'Director, Model United Nations',
+        imageUrl:anav,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:10,
+        name:'Kartikeya Rai',
+        position:'Director, Model United Nations',
+        imageUrl:kartikeya,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:11,
+        name:'Arpan Moitra',
+        position:'Director, Parliamentary Debating',
+        imageUrl:arpan,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:12,
+        name:'Pragati Kumar',
+        position:'Director, Parliamentary Debating',
+        imageUrl:pragati,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:13,
+        name:'Shreyans Jain',
+        position:'Director, General Debating',
+        imageUrl:shreyans,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:14,
+        name:'Avil Goel',
+        position:'Director, General Debating',
+        imageUrl:avil,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:15,
+        name:'Aditi Gupta',
+        position:'Director, Literary Department',
+        imageUrl:aditi,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    },
+    {
+        id:16,
+        name:'T. Sai Ganesh',
+        position:'Director, Literary Department',
+        imageUrl:sai,
+        testimonial:'Debsoc has been a roller-coaster for me . I have learnt a lot in the past years and the experiences have been really great . Debsoc not only makes you a good debater but also helps you grow in life. It is indeed a life changing experience.'
+    }
+];
+export default team;
