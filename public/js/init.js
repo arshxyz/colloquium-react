5dde356608e944c452871df8aceed6889654f62e
Arsh Kohli <kohliarsh9@gmail.com>
2022-01-28 13:41:52 +0530

    fixed nav

--- public/js/init.js
+++ public/js/init.js
@@ -51,16 +51,16 @@ jQuery(document).ready(function($) {
       var y = $(window).scrollTop();
       var nav = $("#nav-wrap");
 
-      if (y > h * 0.2 && y < h && $(window).outerWidth() > 768) {
-        nav.fadeOut("fast");
-      } else {
-        if (y < h * 0.2) {
-          nav.removeClass("opaque").fadeIn("fast");
-        } else {
-          nav.addClass("opaque").fadeIn("fast");
-        }
-      }
-    });
+    //   if (y > h * 0.2 && y < h && $(window).outerWidth() > 768) {
+    //     nav.fadeOut("fast");
+    //   } else {
+    //     if (y < h * 0.2) {
+    //       nav.removeClass("opaque").fadeIn("fast");
+    //     } else {
+    //       nav.addClass("opaque").fadeIn("fast");
+    //     }
+    //   }
+    // });
 
     $(".flexslider").flexslider({
       namespace: "flex-",
