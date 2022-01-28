3f858186f8329267411e16dd919acf43cbd408d4
Harsh Kumar <harshbabu0077@gmail.com>
2022-01-29 03:06:36 +0530

    update 3:00 AM

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -138,8 +138,7 @@ function Home(){
                     <img src="./images/team/Rahul.jpg" className="presidents" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}}/>
                     <h3>A Revolutionary Journey</h3>
        <p>DebSoc NSUT is the breath of fresh air that you need in a world 
-         masqueraded by an obsession over numbers and algorithms. I can tr
-         uly say that your journey with DebSoc would revolutionise your perso
+         masqueraded by an obsession over numbers and algorithms.<br/> I can truly say that your journey with DebSoc would revolutionise your perso
          nality and change your perspective towards things for the good.<br/> DebSoc has
           given me a community with whom I’ll 
          share a bond for the rest of my life and countless memories that will always be embedded in my heart.</p>
